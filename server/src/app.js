7c4f2e3f8ac72a775dff38afbdc28f95759df58e
Harsh Arya <harsharya8946@gmail.com>
2026-05-15 23:16:47 +0530

    post API & react folder setup

--- server/src/app.js
+++ server/src/app.js
@@ -4,4 +4,22 @@ const registerUserModel = require('./models/register.model')
 
 app.use(express.json())
 
+
+app.post('/api/user', async(req,res) => {
+    const {firstName , lastName , dob , email , gender} = req.body
+
+    const users = await registerUserModel.create({
+        firstName,
+        lastName,
+        dob,
+        email,
+        gender
+    })
+
+    res.status(201).json({
+        message : 'employee added successfully',
+        users
+    })
+})
+
 module.exports = app
