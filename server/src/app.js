61059af0f51b6bcea81f6379bf4917ad7002c34a
Harsh Arya <harsharya8946@gmail.com>
2026-05-17 22:56:23 +0530

    delete API - removing user

--- server/src/app.js
+++ server/src/app.js
@@ -3,11 +3,11 @@ const app = express()
 const registerUserModel = require('./models/register.model')
 const cors = require('cors')
 
-
+//middlewares
 app.use(cors())
 app.use(express.json())
 
-
+//post API
 app.post('/api/user', async(req,res) => {
     const {firstName , lastName , dob , email , gender} = req.body
 
@@ -25,12 +25,25 @@ app.post('/api/user', async(req,res) => {
     })
 })
 
-
+//get API
 app.get('/api/user', async(req,res) => {
 
     const users = await registerUserModel.find()
 
     res.status(200).json({
+        users
+    })
+})
+
+//delete API
+app.delete('/api/user/:id', async(req,res) => {
+
+    const id = req.params.id
+
+    const users = await registerUserModel.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message : 'Deleted Successfully',
         users
     })
 })
