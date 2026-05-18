44246130cdc6dbf528da48246eee2d33a08a7660
Harsh Arya <harsharya8946@gmail.com>
2026-05-18 23:46:23 +0530

    UI changes in form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -67,61 +67,97 @@ const App = () => {
 
           <form
             onSubmit={submitHandler}
-            className='flex flex-col gap-5 p-10 w-fit outline-1 rounded-xl'
-          >
+            className='flex flex-col gap-5 p-10 w-fit outline-1 rounded-xl'>
 
-            <h1 className='text-center font-bold text-2xl'>
-              Add Employee
+            <h1 className='font-bold text-3xl text-gray-600'>
+            Register Employee
             </h1>
 
+            <div className='flex flex-col'>
+            <label
+            className='font-semibold text-xl' 
+            htmlFor='firstName'>First Name</label>
             <input
+              id='firstName'
               type="text"
               name="firstName"
               placeholder='Enter First Name'
               className='outline-1 p-2 rounded w-72'
               value={formData.firstName}
               onChange={handleChange}
+              required
             />
+            </div>
 
+            <div className='flex flex-col'>
+            <label
+            className='font-semibold text-xl' 
+            htmlFor='lastName'>Last Name</label>
             <input
+             id='lastName'
               type="text"
               name="lastName"
               placeholder='Enter Last Name'
               className='outline-1 p-2 rounded'
               value={formData.lastName}
               onChange={handleChange}
+              required
             />
+            </div>
 
+            <div className='flex flex-col'>
+            <label
+            className='font-semibold text-xl' 
+            htmlFor='dob'>Date of Birth</label>
             <input
+              id='dob'
               type="date"
               name="dob"
               className='outline-1 p-2 rounded'
               value={formData.dob}
               onChange={handleChange}
+              required
             />
+            </div>
 
+
+            <div className='flex flex-col'>
+            <label
+            className='font-semibold text-xl' 
+            htmlFor='email'>Email</label>
             <input
+              id='email'
               type="email"
               name="email"
               placeholder='Enter Email'
               className='outline-1 p-2 rounded'
               value={formData.email}
               onChange={handleChange}
+              required
             />
+            </div>
 
+            <div className='flex flex-col'>
+            <label
+            className='font-semibold text-xl' 
+            htmlFor='gender'>Gender</label>
             <select
+              id='gender'
               name="gender"
               className='outline-1 p-2 rounded'
               value={formData.gender}
               onChange={handleChange}
+              required
             >
               <option value="">Select Gender</option>
               <option value="male">Male</option>
               <option value="female">Female</option>
               <option value="other">Other</option>
             </select>
+            </div>
 
-            <button className='bg-green-500 text-white p-2 rounded-xl'>
+            <button 
+            className='bg-green-500 text-white p-2 rounded-xl font-bold text-xl'>
               Submit
             </button>
 
@@ -132,17 +168,12 @@ const App = () => {
         <div className='flex flex-wrap gap-4 p-4'>
           {empData.map((elem, idx) => {
             return (
-              <div
-                key={idx}
-                className='bg-gray-700 text-white font-semibold text-md p-4 w-72 rounded-xl h-fit'
-              >
+              <div key={idx}
+              className='bg-gray-700 text-white font-semibold text-md p-4 w-72 rounded-xl h-fit'>
                 <p>First Name - {elem.firstName}</p>
                 <p>Last Name - {elem.lastName}</p>
                 <p>
-                  D.O.B - {
-                    new Date(elem.dob)
-                      .toLocaleDateString('en-GB')
-                  }
+                  D.O.B - {new Date(elem.dob).toLocaleDateString('en-GB')}
                 </p>
                 <p>Gender - {elem.gender}</p>
                 <p>Email - {elem.email}</p>
